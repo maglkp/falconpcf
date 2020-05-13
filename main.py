027fb5794cf079a09ca6a736611d7117eb3bf780
koziol <lukasz.koziol@dell.com>
2020-05-13 15:51:37 +0200

    logs

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import falcon
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 class TestResource(object):
     def on_get(self, req, res):
         """Handles all GET requests."""
+        print(str(req.media))
+        print(str(type(req.media)))
+        logging.info("received a GET")
         res.status = falcon.HTTP_200  # This is the default status
         res.body = ('This is me, Falcon, serving a resource!')
 
